a5c54a19ac401089e661906a6841cd84f84575cd
Iurii Sylaiev <yustas.silaev@gmail.com>
2024-05-25 12:03:27 +0300

    initial commit

--- Calculator/obj/Release/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Release/net8.0/Calculator.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a73f5a9651e26c8dbec03a706ea592b6a8cab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
