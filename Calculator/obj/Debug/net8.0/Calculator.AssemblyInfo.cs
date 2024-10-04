3c5e1b0fb85dd94fa834c6e5e06126dfc970be8f
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-10-04 20:55:02 +0300

    start

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9647f53705fcad6cd7590d3ab97575a762b8570")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e440010ced9464d9cda08476df1d508ce9143c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
